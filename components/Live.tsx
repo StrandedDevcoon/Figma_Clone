16ed4c94a0a0c5791533aeefdd0c516f84d77eac
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2024-02-19 11:45:24 +0100

    [TASK] Add Live Cursor functionality

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,9 +1,43 @@
 import LiveCursors from "@/components/cursor/LiveCursors";
+import {useMyPresence, useOthers} from "@/liveblocks.config";
+import { useCallback } from "react";
 
 const Live = () => {
+    const others = useOthers();
+    const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+    const handlePointerMove = useCallback((event: React.PointerEvent) => {
+        event.preventDefault();
+
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+        updateMyPresence({ cursor: { x, y } });
+    }, [])
+
+    const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+        event.preventDefault();
+
+        updateMyPresence({ cursor: null, message: null });
+    }, [])
+
+    const handlePointerDown = useCallback((event: React.PointerEvent) => {
+
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+        updateMyPresence({ cursor: { x, y } });
+    }, [])
+
     return (
-        <div>
-            <LiveCursors />
+        <div
+            onPointerMove={handlePointerMove}
+            onPointerLeave={handlePointerLeave}
+            onPointerDown={handlePointerDown}
+            className="h-[100vh] w-full flex justify-center items-center text-center"
+        >
+            <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+            <LiveCursors others={others} />
         </div>
     )
 }
--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -1,6 +1,20 @@
-const Cursor = () => {
+import CursorSVG from "@/app/assets/CursorSVG";
+
+type Props = {
+    color: string;
+    x: number;
+    y: number;
+    message?: string;
+}
+
+const Cursor = ({ color, x, y, message }: Props) => {
     return (
-        <div>Cursor</div>
+        <div
+            className="pointer-events-none absolute top-0 left-0"
+            style={{ transform: `translateX(${x}px) translateY(${y}px)`}}>
+            <CursorSVG color={color} />
+
+        </div>
     )
 }
 
