a2e7e8083283ca45c16d6f27e701bed7315b01b6
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2024-02-19 10:20:36 +0100

    [TASK] Add Cursor Skeleton

---
+++ components/Live.tsx
@@ -0,0 +1,11 @@
+import LiveCursors from "@/components/cursor/LiveCursors";
+
+const Live = () => {
+    return (
+        <div>
+            <LiveCursors />
+        </div>
+    )
+}
+
+export default Live
