9d350e3a7aa1567f9b4b718fecfe545113538988
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2024-02-19 19:06:17 +0100

    [Task] Add Reaction Effects

--- components/Live.tsx
+++ components/Live.tsx
@@ -2,23 +2,28 @@ import LiveCursors from "@/components/cursor/LiveCursors";
 import {useMyPresence, useOthers} from "@/liveblocks.config";
 import React, {useCallback, useEffect, useState} from "react";
 import CursorChat from "@/components/cursor/CursorChat";
-import {CursorMode} from "@/types/type";
+import {CursorMode, CursorState, Reaction} from "@/types/type";
+import ReactionSelector from "@/components/reaction/ReactionButton";
 
 const Live = () => {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
-    const [cursorState, setCursorState] = useState({
+    const [cursorState, setCursorState] = useState<CursorState>({
         mode: CursorMode.Hidden,
     })
+
+    const [reactions, setReactions] = useState<Reaction[]>([]);
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
 
-        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-        const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+        if (cursor == null || cursorState.mode !== CursorMode.ReactionSelector) {
+            const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+            const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-        updateMyPresence({ cursor: { x, y } });
+            updateMyPresence({cursor: {x, y}});
+        }
     }, [])
 
     const handlePointerLeave = useCallback((event: React.PointerEvent) => {
@@ -26,13 +31,19 @@ const Live = () => {
         updateMyPresence({ cursor: null, message: null });
     }, [])
 
+    const handlePointerUp = useCallback((event: React.PointerEvent) => {
+        setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: false } : state);
+    }, [cursorState.mode, setCursorState])
+
     const handlePointerDown = useCallback((event: React.PointerEvent) => {
 
         const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
         updateMyPresence({ cursor: { x, y } });
-    }, [])
+
+        setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: true } : state);
+    }, [cursorState.mode, setCursorState])
 
     useEffect(() => {
         const onKeyUp = (e: KeyboardEvent) => {
@@ -45,6 +56,10 @@ const Live = () => {
             } else if (e.key === 'Escape') {
                 updateMyPresence({ message: '' })
                 setCursorState({ mode: CursorMode.Hidden})
+            } else if (e.key === 'e') {
+                setCursorState({
+                    mode: CursorMode.ReactionSelector,
+                })
             }
         }
         const onKeyDown = (e: KeyboardEvent) => {
@@ -66,6 +81,7 @@ const Live = () => {
         <div
             onPointerMove={handlePointerMove}
             onPointerLeave={handlePointerLeave}
+            onPointerUp={handlePointerUp}
             onPointerDown={handlePointerDown}
             className="h-screen w-full flex justify-center items-center text-center"
         >
@@ -76,6 +92,17 @@ const Live = () => {
                     cursorState={cursorState}
                     setCursorState={setCursorState}
                     updateMyPresence={updateMyPresence}
+                />
+            )}
+            {cursorState.mode === CursorMode.ReactionSelector && (
+                <ReactionSelector
+                    setReaction={(reaction) => {
+                        setCursorState({
+                            mode: CursorMode.Reaction,
+                            reaction,
+                            isPressed: false
+                        })
+                    }}
                 />
             )}
             <LiveCursors others={others}
