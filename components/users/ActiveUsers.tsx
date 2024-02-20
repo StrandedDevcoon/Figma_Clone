2b3f1be8739479780b89359e3fbbbf2ab04b7bae
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2024-02-20 22:43:55 +0100

    [TASK] Add active User Avatars AND [FIXED] Issue closing chat and open Reactions by pressing 'e'

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -3,28 +3,32 @@ import {Avatar} from "@/components/users/Avatar";
 
 import styles from './index.module.css'
 import {generateRandomName} from "@/lib/utils";
+import {useMemo} from "react";
 
 const ActiveUsers = () => {
     const users = useOthers();
     const currentUser = useSelf();
     const hasMoreUsers = users.length > 3;
 
-    return (
-        <main className="flex h-screen w-full select-none place-content-center place-items-center">
-            <div className="flex pl-3">
-                {currentUser && (
-                    <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
-                )}
-                {users.slice(0, 3).map(({ connectionId, info }) => {
-                    return (
-                        <Avatar key={connectionId} name={generateRandomName()} otherStyles="-ml-3"/>
-                    );
-                })}
-
-                {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
+    const memoizedUsers = useMemo(() => {
+        return (
+            <div className="flex items-center justify-center gap-1 py-2">
+                <div className="flex pl-3">
+                    {currentUser && (
+                        <Avatar name="You" otherStyles="border-[3px] border-primary-green"/>
+                    )}
+                    {users.slice(0, 3).map(({connectionId}) => {
+                        return (
+                            <Avatar key={connectionId} name={generateRandomName()} otherStyles="-ml-3"/>
+                        );
+                    })}
+                    {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
+                </div>
             </div>
-        </main>
-    );
+        );
+    }, [users.length])
+
+    return memoizedUsers;
 }
 
 export default ActiveUsers
