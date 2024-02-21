69438b9023336040eb10d441db712118eb76890b
Titas Siauciunas <115973260+StrandedDevcoon@users.noreply.github.com>
2024-02-21 18:53:05 +0100

    [TASK] Apply suggestions from @iven.s
    Co-authored-by: Iven Schlenther <iven.s@lvckyworld.net>

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -2,7 +2,7 @@ const RightSidebar = () => {
     return (
         <section
             className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300
-            min-2-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20"
+            min-w-56 sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20"
         >
             <h3 className="px-5 pt-4 text-xs uppercase">
                 Design
