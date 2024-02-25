db457b6d49f1e49d65fe7329f7a4cd0c21f4dc75
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2024-02-25 18:22:55 +0100

    [TASK] Add skeleton for right sidebar functions

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,12 +1,26 @@
+import Dimensions from "@/components/settings/Dimensions";
+import Color from "@/components/settings/Color";
+import Export from "@/components/settings/Export";
+import Text from "@/components/settings/Text";
+
 const RightSidebar = () => {
     return (
         <section
             className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300
-            min-w-56 sticky right-0 h-full max-sm:hidden select-none overflow-y-auto pb-20"
+            min-w-56 sticky right-0 h-full max-sm:hidden select-none"
         >
             <h3 className="px-5 pt-4 text-xs uppercase">
                 Design
             </h3>
+            <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+                Make changes to canvas as you like
+            </span>
+
+            <Dimensions />
+            <Text />
+            <Color />
+            <Color />
+            <Export />
         </section>
     )
 }
